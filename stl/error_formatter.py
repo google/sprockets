0ff15b58f7b26128e1d0525e51cd64691027d69a
Michael Bjorge <mike.bjorge@gmail.com>
2017-07-11 15:25:49 -0700

    Improve error messaging. (#14)
    * Improve error messaging.
    
    As a first step in improved error messaging, setup a system
    for pretty printing the parser errors. And make the error formatters
    replaceable. This makes it easy to dump out computer readable formats
    for use in build systems and human readable formats for manual use.
    
    Issue: #2
    Test: Add syntex error to example/example_base.stl and try to parse it.
    
    * Update parser.py
    
    Fix some bad formatting that happened
    
    * Address comments in error_formatter.py
    
    * Address comments for error_handler.py
    
    * Addressed comments in parser.py
    
    * Fix syntax error in error_formatter.py
    
    * Fix indentation
    
    * Fix copyright year
    
    * Move ErrorInfo and ErrorPosition
    
    * Move ErrorInfo and ErrorPostiion
    
    * add missing import
    
    * remove unused import

---
+++ stl/error_formatter.py
@@ -0,0 +1,154 @@
+# Copyright 2017 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module for formatting parser/syntax errors."""
+
+import abc
+import collections
+import json
+import re
+
+
+ErrorInfo = collections.namedtuple('ErrorInfo', ['id', 'filename', 'line', 'position', 'message'])
+# Args:
+#   id: An identifier for the type of error.
+#   filename: Name of the file the error occured in.
+#   line: The text of the line the error occured in.
+#   position: An ErrorPosition pointing to where the error occured.
+#   messasge: The human readable message explaining the error
+
+ErrorPosition = collections.namedtuple('ErrorPosiion', ['line', 'start', 'end'])
+# Args:
+#   line: The line number of the error.
+#   start: The position (column) the error starts at in the line (inclusive).
+#   end: The position (column) the error ends at in the line (inclusive).
+
+
+class ErrorFormatter(object):
+  """Interface for displaying STL parsing errors."""
+  __metaclass__ = abc.ABCMeta
+
+  @abc.abstractmethod
+  def Format(self, error):
+    """Outputs an ErrorInfo |error| in a custom format."""
+    pass
+
+
+class JsonErrorFormatter(ErrorFormatter):
+  """Format errors as json objects."""
+
+  def Format(self, error):
+    """Format |error| as json."""
+    return json.dumps(error._asdict())
+
+
+class Color(object):
+  """ANSI escape code base color codes.
+
+  See https://en.wikipedia.org/wiki/ANSI_escape_code#Colors for more info.
+  """
+
+  BLACK = 0
+  RED = 1
+  GREEN = 2
+  YELLOW = 3
+  BLUE = 4
+  MAGENTA = 5
+  CYAN = 6
+  WHITE = 7
+  DEFAULT = 9
+  
+  @staticmethod
+  def _IsBaseColor(color):
+    return color in [Color.BLACK,
+                     Color.RED,
+                     Color.GREEN,
+                     Color.YELLOW,
+                     Color.BLUE,
+                     Color.MAGENTA,
+                     Color.CYAN,
+                     Color.WHITE,
+                     Color.DEFAULT]
+
+  @staticmethod
+  def Foreground(color):
+    """Converts |color| into its foreground color code."""
+    if not Color._IsBaseColor(color):
+      raise ValueError('Expected a Color.COLOR constant, found: {}'.format(color))
+    return color + 30
+  
+  @staticmethod
+  def Background(color):
+    """Converts |color| into its background color code."""
+    if not Color._IsBaseColor(color):
+      raise ValueError('Expected a Color.COLOR constant, found: {}'.format(color))  
+    return color + 40  
+
+
+class PrettyErrorFormatter(ErrorFormatter):
+  """Pretty-ify errors with colors, highlights, and helpful markings."""
+
+  _ERROR_COLOR = Color.RED
+  _ANNOTATION_COLOR = Color.YELLOW
+
+  def _Colorize(self,
+               text,
+               foreground=Color.DEFAULT,
+               background=Color.DEFAULT,
+               bold=True):
+    bold = int(bold)
+    fg_color = Color.Foreground(foreground) 
+    bg_color = Color.Background(background)
+    return '\x1b[{bold};{fg_color};{bg_color}m{text}\x1b[0m'.format(
+        text=text, bold=bold, fg_color=fg_color, bg_color=bg_color)
+
+  def _GetMessageLine(self, error):
+    """Returns the formatted line with the error message."""
+    file_position = '{}:{}:{}'.format(
+        error.filename, error.position.line, error.position.start)
+    return '{}({}): {}'.format(
+        self._Colorize('error', self._ERROR_COLOR),
+        self._Colorize(file_position, self._ANNOTATION_COLOR, bold=False),
+        error.message)
+
+  def _GetSourceLine(self, error):
+    """Returns the formatted line from the source that caused the error."""
+    return '{}{}'.format(self._GetLineNumberPrefix(error), error.line)
+
+  def _GetSourceAnnotatinoLine(self, error):
+    """Returns a line that highlights the original source."""
+    pre_annotation = ' ' * error.position.start
+    annotation = '^' * (error.position.end - error.position.start + 1)
+    return '{}{}'.format(
+        self._GetLineNumberPrefix(error, show_number=False),
+        self._Colorize(pre_annotation + annotation, self._ANNOTATION_COLOR))
+
+  def _GetLineNumberPrefix(self, error, show_number=True):
+    """Returns a prefix to annotate a line with a line number.
+
+    Args:
+      error: The ErrorInfo to get the line number from.
+      show_number: Whether to show or hide the number (hiding the number is
+          useful to get a prefix with the same width and formatting).
+    """
+    prefix_with_number = ' {} | '.format(error.position.line)
+    if show_number:
+      return prefix_with_number
+    return re.sub('\d', ' ', prefix_with_number)
+  
+  def Format(self, error):
+    lines = [self._GetMessageLine(error),
+             self._GetSourceLine(error),
+             self._GetSourceAnnotationLine(error),]
+    return '\n'.join(lines)
---
+++ stl/error_handler.py
@@ -0,0 +1,74 @@
+# Copyright 2017 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module for handling errors from parsing an STL file."""
+
+import error_formatter
+
+
+class ParserErrorHandler(object):
+  """Determines the likely cause of a parse error."""
+
+  def __init__(self, error_formatter):
+    """Creates a ParserError instance.
+
+    Args:
+      error_formatter: An ErrorFormatter to format the parse errors.
+    """
+    self._formatter = error_formatter
+
+  def Format(self, error):
+    """Formats |error| into a string."""
+    return self._formatter.Format(error)
+
+  def _GetColumn(self, data, token):
+    """Gets the column that |token| starts at within a line in |data|."""
+    last_newline = data.rfind('\n', 0, token.lexpos)
+    if last_newline < 0:
+      last_newline = 0
+    column = token.lexpos - last_newline
+    return column
+
+  def _GetLine(self, data, token):
+    """Gets the line of text that |token| is part of in |data|."""
+    start_line_pos = data.rfind('\n', 0, token.lexpos) + 1
+    end_line_pos = data.find('\n', start_line_pos)
+    return data[start_line_pos:end_line_pos]
+
+
+  def GetError(self, filename, parser, lexer):
+    """Returns an error string based on the state of |parser| and |lexer".
+
+    Args:
+      filename: The name of the file the error occured in.
+      parser: A PLY parser that has reached the p_error() state.
+      lexer: The PLY lexer that |parser| used to lex.
+
+    Returns:
+      A formatted error string.
+    """
+    print 'SymStack: {}'.format(parser.symstack)
+    final_token = parser.symstack[-1]
+    error_start_column = self._GetColumn(lexer.lexdata, final_token) - 1
+    error_end_column = error_start_column + len(final_token.value) - 1
+    error_position = error_formatter.ErrorPosition(line=lexer.lineno,
+                                                   start=error_start_column,
+                                                   end=error_end_column)
+    error = error_formatter.ErrorInfo(
+        id=0,
+        filename=filename,
+        line=self._GetLine(lexer.lexdata, final_token),
+        position=error_position,
+        message='There was a parsing error :(')
+    return self.Format(error)
